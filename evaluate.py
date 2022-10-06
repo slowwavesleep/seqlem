0fc75670acf35d07093ad60772e87ea36b378212
slowwavesleep <44175589+slowwavesleep@users.noreply.github.com>
2022-10-06 13:34:29 +0300

    Add evaluation

---
+++ evaluate.py
@@ -0,0 +1,13 @@
+import conllu
+
+with open("et_edt-ud-test.conllu") as file:
+    data = file.read()
+    parsed = conllu.parse(data)
+
+with open("test_preds.txt") as file:
+    preds = []
+    for line in file:
+        preds.append(line.split(" "))
+
+print(len(preds))
+print(len(parsed))
