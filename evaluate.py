3aa471ed475442f8091c790434505c1745fd3119
slowwavesleep <44175589+slowwavesleep@users.noreply.github.com>
2022-10-06 13:42:07 +0300

    Debug evaluate

--- evaluate.py
+++ evaluate.py
@@ -9,11 +9,12 @@ with open("test_preds.txt") as file:
     for line in file:
         preds.append(line.split(" "))
 
-# print(len(preds))
-# print(len(parsed))
 
 for (true_sent, pred_sent) in zip(parsed, preds):
+    print(len(true_sent))
+    print(len(pred_sent))
     assert len(true_sent) == len(pred_sent)
+
     for i, pred in enumerate(pred_sent):
         true_sent[i]["lemma"] = pred
 
