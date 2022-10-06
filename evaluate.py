2a09a9f74fe9fc9b4304180223c777de9b597195
slowwavesleep <44175589+slowwavesleep@users.noreply.github.com>
2022-10-06 13:41:15 +0300

    Update evaluate

--- evaluate.py
+++ evaluate.py
@@ -9,5 +9,13 @@ with open("test_preds.txt") as file:
     for line in file:
         preds.append(line.split(" "))
 
-print(len(preds))
-print(len(parsed))
+# print(len(preds))
+# print(len(parsed))
+
+for (true_sent, pred_sent) in zip(parsed, preds):
+    assert len(true_sent) == len(pred_sent)
+    for i, pred in enumerate(pred_sent):
+        true_sent[i]["lemma"] = pred
+
+with open("predicted.conllu", "w") as file:
+    file.writelines([sentence.serialize() + "\n" for sentence in parsed])
