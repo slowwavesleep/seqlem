95176e67863e24cab1d768532cf42f9d65cb00b8
slowwavesleep <44175589+slowwavesleep@users.noreply.github.com>
2022-10-18 16:48:26 +0300

    Remove special symbols

--- compare_connll.py
+++ compare_connll.py
@@ -3,6 +3,8 @@ from conllu import parse
 gold_file_path = "et_edt-ud-dev.conllu"
 pred_file_path = "predicted.conllu"
 
+REMOVE_SYMBOLS = True
+
 with open(gold_file_path) as file:
     gold_conll = parse(file.read())
 
@@ -14,10 +16,13 @@ correct = 0
 
 for pred_token_list, gold_token_list in zip(pred_conll, gold_conll):
     for pred_token, gold_token in zip(pred_token_list, gold_token_list):
-        if pred_token["lemma"] == gold_token["lemma"]:
+        true_lemma = gold_token["lemma"]
+        if REMOVE_SYMBOLS:
+            true_lemma = true_lemma.replace("_", "").replace("=", "")
+        if pred_token["lemma"] == true_lemma:
             correct += 1
         else:
-            print(f'{pred_token["lemma"]} != {gold_token["lemma"]}')
+            print(f'{pred_token["lemma"]} != {true_lemma}')
         total += 1
 
 print(correct / total)
