d62e4ce6519c8a4ca9c79531e672bd4e9da5026a
slowwavesleep <44175589+slowwavesleep@users.noreply.github.com>
2022-10-28 12:44:52 +0300

    Debug labels

--- debug_labels.py
+++ debug_labels.py
@@ -23,7 +23,7 @@ with open(PRED_LEMMAS_PATH) as file:
 
 for lemma_sent, label_sent in zip(pred_lemmas, pred_labels):
     true_label_length = len([label for label in label_sent if label != -100])
-    print(true_label_length, len(lemma_sent))
+    print(len(label_sent), true_label_length, len(lemma_sent))
 
 
 total = 0
