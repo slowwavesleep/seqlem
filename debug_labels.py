761c37605d849b358a6bb7c16a07dff7982366a5
slowwavesleep <44175589+slowwavesleep@users.noreply.github.com>
2022-10-28 12:54:09 +0300

    Debug labels

--- debug_labels.py
+++ debug_labels.py
@@ -1,10 +1,24 @@
+from conllu import parse
+
 TRUE_LABELS_PATH = "true_labels.txt"
 PRED_LABELS_PATH = "predicted_labels.txt"
 PRED_LEMMAS_PATH = "test_preds.txt"
+TRUE_LEMMAS_PATH = "et_edt-ud-dev.conllu"
 
 true_labels = []
 pred_labels = []
 pred_lemmas = []
+
+with open(TRUE_LEMMAS_PATH) as file:
+    gold_conll = parse(file.read())
+
+true_lemmas = []
+
+for sent in true_lemmas:
+    tmp = []
+    for token in sent:
+        tmp.append(token["lemma"])
+    true_lemmas.append(tmp)
 
 with open(TRUE_LABELS_PATH) as file:
     for line in file:
@@ -23,16 +37,14 @@ with open(PRED_LEMMAS_PATH) as file:
 
 equal_lengths = []
 
-for lemma_sent, label_sent, true_sent in zip(pred_lemmas, pred_labels, true_labels):
+for true_lemma_sent, pred_lemma_sent, label_sent, true_sent in zip(true_lemmas, pred_lemmas, pred_labels, true_labels):
     true_preds = []
     for p, t in zip(label_sent, true_sent):
         if t != -100:
             true_preds.append(p)
-    equal_lengths.append(len(true_preds) == len(lemma_sent))
+    equal_lengths.append(len(true_lemma_sent) == len(true_preds) == len(pred_lemma_sent))
 
 print(all(equal_lengths))
-
-
 
 total = 0
 correct = 0
