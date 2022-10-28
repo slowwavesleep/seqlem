186175f3a36bad180a04e2ee9dbe033c6e3b9339
slowwavesleep <44175589+slowwavesleep@users.noreply.github.com>
2022-10-28 12:41:31 +0300

    Debug labels

--- debug_labels.py
+++ debug_labels.py
@@ -1,8 +1,10 @@
 TRUE_LABELS_PATH = "true_labels.txt"
 PRED_LABELS_PATH = "predicted_labels.txt"
+PRED_LEMMAS_PATH = "test_preds.txt"
 
 true_labels = []
 pred_labels = []
+pred_lemmas = []
 
 with open(TRUE_LABELS_PATH) as file:
     for line in file:
@@ -13,6 +15,18 @@ with open(PRED_LABELS_PATH) as file:
     for line in file:
         labels = line.strip("\n").split(" ")
         pred_labels.append([int(label) for label in labels])
+
+with open(PRED_LEMMAS_PATH) as file:
+    for line in file:
+        lemmas = line.strip("\n").split(" ")
+        pred_lemmas.append(lemmas)
+
+equal_lengths = []
+for lemma_sent, label_sent in zip(pred_lemmas, pred_labels):
+    true_label_length = len([label for label in label_sent if label != -100])
+    equal_lengths.append(len(lemma_sent) == true_label_length)
+
+print(all(equal_lengths))
 
 total = 0
 correct = 0
